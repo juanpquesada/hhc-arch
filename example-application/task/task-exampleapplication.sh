3c75f588c3e6b7fed65ab95be0af69d6c83c20b0
juanpquesada <57990524+juanpquesada@users.noreply.github.com>
2024-12-02 22:22:45 +0000

    Add files via upload

--- example-application/task/task-exampleapplication.sh
+++ example-application/task/task-exampleapplication.sh
@@ -1,1 +1,29 @@
+#!/bin/bash
 
+# Upload data required by the task to the S3 bucket
+python3 ~/Desktop/hhc-arch/example-application/task/upload_data.py
+
+# Store authentication data and the API id in variables
+api_id='<API_GATEWAY_API_ID>'
+client_id='<AWS_CLIENT_ID>'
+password='<PASSWORD>'
+user='<USER>'
+
+# Get the authorization token (id token) for the user
+id_token=$(aws cognito-idp initiate-auth \
+	--auth-flow USER_PASSWORD_AUTH \
+	--auth-parameters USERNAME=$user,PASSWORD=$password \
+	--client-id $client_id \
+	| jq '.AuthenticationResult.IdToken')
+len=$((${#id_token}-2))
+id_token=${id_token:1:$len}
+ 
+# Send the task (invoking the action created in the API)
+# num_nodes = (64+1) * (64+1) = 4225
+curl \
+    --data '{ "type": "task", "task": { "name": "taskexampleapplication", "procs": [ { "id": 1, "name": "coons", "params": {"parametricData": "http://s3.amazonaws.com/<BUCKET>/tangled-parametric.msh", "boundaryData": "http://s3.amazonaws.com/<BUCKET>/tangled-physical.msh"}, "dependsOn": [] }, { "id": 2, "dependsOn": [1], "if": "Float({http://s3.amazonaws.com/<BUCKET>/tanglednodes-taskexampleapplication-p1})/4225 >= 0.01", "then": [ { "id": 3, "name": "laplacian", "params": {"parametricData": "http://s3.amazonaws.com/<BUCKET>/tangled-parametric.msh", "boundaryData": "http://s3.amazonaws.com/<BUCKET>/coons-taskexampleapplication-p1.msh", "epsilon": 0.01, "maxIter": 300}, "dependsOn": [] } ] } ], "outProcs": [1, 3], "notificationEmail": "<EMAIL>" } }' \
+	--header "Authorization: $id_token" \
+	--header 'Content-Type: application/json' \
+	--location \
+	--request POST \
+    "https://$api_id.execute-api.us-east-1.amazonaws.com/prod/send-task"
--- example-application/task/upload_data.py
+++ example-application/task/upload_data.py
@@ -1,1 +1,41 @@
+#
+# hhc-arch/example-application/tasks/task-exampleapplication/upload_data.py
+#
+import boto3
+import os
+import requests
+import time
 
+# AWS credentials and some useful constant values
+AWS_ACCESS_KEY_ID      = 'AWS_ACCESS_KEY_ID>'
+AWS_SECRET_ACCESS_KEY  = '<AWS_SECRET_ACCESS_KEY>'
+AWS_REGION             = 'us-east-1'
+AWS_S3_BUCKET          = '<BUCKET>'
+AWS_S3_SERVICE_URL     = 's3.amazonaws.com'
+
+text = """
+Task:
+  epsilon = 0.01
+  parametric_mesh = 'tangled-parametric.msh'
+  physical_mesh   = 'tangled-physical.msh'
+  num_tangled_nodes = coons(parametric_mesh, physical_mesh)
+  num_nodes = (64+1) * (64+1)
+  if Float(num_tangled_nodes)/num_nodes >= epsilon
+    max_iter = 300
+    num_tangled_nodes = laplacian(parametric_mesh, physical_mesh, epsilon, max_iter)
+  end
+
+"""
+print(text)
+
+# Get the S3 client
+s3 = boto3.client(service_name='s3', region_name=AWS_REGION, aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
+
+# Upload the meshes
+parametric_mesh = 'tangled-parametric.msh'
+s3.upload_file(parametric_mesh, AWS_S3_BUCKET, parametric_mesh, ExtraArgs={'ACL': 'public-read'})
+
+physical_mesh = 'tangled-physical.msh'
+s3.upload_file(physical_mesh, AWS_S3_BUCKET, physical_mesh, ExtraArgs={'ACL': 'public-read'})
+
+time.sleep(0.5)
