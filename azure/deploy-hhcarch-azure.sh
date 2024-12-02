6d7b69ee10ac5f60f1322e797d5f475634a3492b
juanpquesada <57990524+juanpquesada@users.noreply.github.com>
2024-12-02 21:56:43 +0000

    Add files via upload

--- azure/deploy-hhcarch-azure.sh
+++ azure/deploy-hhcarch-azure.sh
@@ -1,1 +1,31 @@
+#!/bin/bash
 
+# Login
+az login \
+	--username <USERNAME>
+
+# Create a resource group
+az group create \
+    --location westeurope \
+    --name <RESOURCE_GROUP_NAME>
+
+# Create a storage account
+az storage account create \
+    --location westeurope \
+    --name <STORAGE_ACCOUNT_NAME> \
+    --resource-group <RESOURCE_GROUP_NAME> \
+    --sku Standard_LRS
+
+# Get the access key for the storage account
+ACCESS_KEY=$(az storage account keys list \
+	--account-name <STORAGE_ACCOUNT_NAME> \
+    --resource-group <RESOURCE_GROUP_NAME> \
+	| jq '.[0].value')
+len=$((${#ACCESS_KEY}-2))
+ACCESS_KEY=${ACCESS_KEY:1:$len}
+sed -i '' \
+	"s|AZURE_STORAGE_ACCESS_KEY = '.*'|AZURE_STORAGE_ACCESS_KEY = '${ACCESS_KEY}'|g" \
+	~/Desktop/hhc-arch/aws/dispatcher/dispatcher.inc.rb
+
+# Logout
+az logout
--- azure/release-hhcarch-azure.sh
+++ azure/release-hhcarch-azure.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
 
+# Login
+az login \
+	--username <USERNAME>
+
+# Delete deployed resources
+az group delete \
+	--name <RESOURCE_GROUP_NAME>
+
+# Logout
+az logout
